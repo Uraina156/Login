8daee68245c3db84c78add689c7df8f15cc9e3ff
Uraina156 <uraina1998@gmail.com>
2020-08-20 14:55:06 +0700

    first commit

---
+++ lib/screens/radio.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class radio extends StatefulWidget {
+ radio({Key key, this.title}) : super(key: key); 
+final String title;
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
